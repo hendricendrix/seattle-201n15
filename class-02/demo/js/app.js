6fc16e31bf8a2e5a6e0edd0298f537fb4bf6a27a
LENA EIVY <applena@gmail.com>
2019-08-12 21:36:58 -0700

    day 2 updated

---
+++ class-02/demo/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var likesGameOfThrones = prompt('Do you like Game Of Thrones?');
+// likesGameOfThrones = likesGameOfThrones.toLowerCase();
+
+if(likesGameOfThrones.toUpperCase() === 'YES'){
+  alert('I like Game Of Thrones, Too');
+} else {
+  alert('What do you mean?!');
+}
+
+// if(condition is met){
+//   do something
+// } else if(a differnt condition is met){
+//   do something else
+// } else {
+//   do something else
+// }
